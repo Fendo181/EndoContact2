50556be2be59d23a77c1d7ff71cdb074017e9526
Fendo181 <fendo181git@gmail.com>
2017-07-02 16:19:03 +0000

    errorハンドリングを変更した。

--- resources/views/linklist/submit.blade.php
+++ resources/views/linklist/submit.blade.php
@@ -9,26 +9,24 @@
         <div class="form-groupe">
             <label for="title">Title</label>
             <input type="text" class="form-control" id="title" name="title" placeholder="Title">
+            @if ($errors->has('title'))
+                <div class="alert alert-danger">{{ $errors->first('title') }}</div>
+            @endif
         </div>
         <div class="form-group">
            <label for="url">Url</label>
            <input type="text" class="form-control" id="url" name="url" placeholder="URL">
+           @if ($errors->has('url'))
+               <div class="alert alert-danger">{{ $errors->first('url') }}</div>
+           @endif
        </div>
        <div class="form-group">
            <label for="description">Description</label>
            <textarea class="form-control" id="description" name="description" placeholder="description"></textarea>
+           @if ($errors->has('description'))
+               <div class="alert alert-danger">{{ $errors->first('description') }}</div>
+           @endif
        </div>
        <button type="submit" class="btn btn-default">Submit</button>
     </form>
-    <!-- errorメッセージ  -->
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-              <ul>
-                  @foreach ($errors->all() as $error)
-                      <li>{{ $error }}</li>
-                  @endforeach
-              </ul>
-        </div>
-    @endif
-</div>
 @endsection
