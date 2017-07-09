8a6da56a7ac32e9e82407d699723a491fd80bbb8
Fendo181 <fendo181git@gmail.com>
2017-07-09 01:27:33 +0000

    Fxi design Code
    1.BootStrapのCDNで呼び出している箇所を外した。
    2.<li>タグの「・」を消した。
    3.インデントを揃えた。

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,13 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
-     <!-- CSS-->
-     <!-- <link rel="stylesheet" href="\css\styeles.css"> -->
      <!-- BootStrap -->
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-     
      <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
+     <!-- CasttomerCSS-->
+     <link rel="stylesheet" href="{{ asset('css/mystyle.css') }}">
 
 </head>
 <body>
@@ -19,27 +16,8 @@
         @yield('blog_title')
     </div>
 
-    {{-- 画像 --}}
-    <div align="center">
-        @yield('img1')
-    </div>
-
-    {{-- リンク ページ --}}
-    <div class="container">
-        @yield('about')@yield('aboutme')@yield('git')@yield('Dot')
-    </div>
-
     {{-- コンテンツの表示 --}}
     <div class="container">
-       @yield('content')
+        @yield('content')
     </div>
-
-    {{-- バックリンク --}}
-    <div class="container">
-        @yield('back')
-    </div>
-<script>
-
-
-</script>
 </body>
